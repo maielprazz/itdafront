f0e5ca0a5f3a9ce00e4c40dcff232256ca4687f3
TeamDataAnalytics <ba.admin@map.co.id>
2021-07-15 13:32:13 +0700

    updated login form

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import './App.css';
+// import './App.css';
+import { Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fab, faCheckSquare, faCoffee);
 
 function App() {
   return (
     <div className="App">
-      <div className="center-column">
-        <div className="item-row">
-          <span>Create Django Project</span>
-        </div>
+      <Form className="mt-3 pt-3">
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="email" placeholder="Enter username" />
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
 
-        <div className="item-row">
-          <span>Create React app: "npx create-react-app appname"</span>
-        </div>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
 
-        <div className="item-row">
-          <span>Drag react app into root directory of django project</span>
-        </div>
-
-        <div className="item-row">
-          <span>Configure TEMPALTES engine</span>
-        </div>
-
-        <div className="item-row">
-          <span>Configure URL path</span>
-        </div>
-
-        <div className="item-row">
-          <span>Configure static files</span>
-        </div>
-
-        <div className="item-row">
-          <span>cd into react app and run "npm run build"</span>
-        </div>
-      </div>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+      <FontAwesomeIcon icon="check-square" />
+      Your <FontAwesomeIcon icon="coffee" /> is hot and ready!
     </div>
   );
 }
